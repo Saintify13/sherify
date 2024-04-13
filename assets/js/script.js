35f1fd2a78e59f6c514da3f33a9744bfdd69222b
Saintify13 <saintify200@yahoo.com>
2024-04-13 01:14:00 +0000

    Add variables for accessing elements in DOM

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//Variables for accessing elements.
+
+let countSpan = document.querySelector('.count span');
+let flagImgDiv = document.querySelector('.flag-img');
+let flagImg = document.querySelector('.flag-img img');
+let flagOptions = document.querySelector('.flag-options ul');
+let flagList = document.querySelectorAll('.flag-options ul li');
+
+let currentIndex = 0;
