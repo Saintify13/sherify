05eb8852e72591aa2decb4ef39ec34b03e7ae199
Saintify13 <saintify200@yahoo.com>
2024-04-17 10:18:04 +0000

    Add checkAnswer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Wait for DOM to finish loading before running the game//
 //Variables for accessing elements.
 
 let countSpan = document.querySelector('.count span');
@@ -11,33 +12,62 @@ let correctAns = document.querySelector('.score .correct span');
 let incorrectAns = document.querySelector('.score .incorrect span');
 let btnNewGame = document.querySelector('#newGame');
 
-
 let currentIndex = 0;
 let correctAnswer = 0;
 
 function getQuestion() {
     let myRequest = new XMLHttpRequest();
     myRequest.onreadystatechange = function() {
-        if (this.readystate === 4 && this.status ===200) {
+        if (this.readystate === 4 && this.status === 200) {
             let questions = JSON.parce(this.responseText);
             // number of questions for each new game
             let qCount = 20;
             questionNum(qCount);
+            //Random questions each new game
+            questions = questions.sort(() => Math.random() - Math.random()).slice(0, 20);
             // Add questions data
             addQuestiondata(questions[currentIndex], qCount);
+    
+            flagList.forEach(li => {
+                li.addEventListener('click', () => {
+                    let correctAnswer = questions[currentIndex].correctAnswer
+                    li.classList.add('active');
+                    //Increase index
+                    currentIndex++;
+    
+                    //Check the answer after 500ms
+                    setTimeout(() => {
+                        checkAnswer(correctAnswer, qCount);
+                    }, 500);
+    
+                    setTimeout(() => {
+                        //Remove previous image source
+                        flagImg.src = '';
+                        //Remove all classes(active, success, incorrect)
+                        li.classList.remove('active');
+                        li.classList.remove('success');
+                        li.classList.remove('incorrect');
+    
+                        //Add question data to show next question
+                        addQuestiondata(questions[currentIndex], qCount);
+                    }, 1000)
+    
+                    //Show results
+                    setTimeout(() => {
+                        showResults(qCount);
+                    }, 1002);
+                });
+            });
         }
     }
-
-    myRequest.open("GET", "js/flag_questions.json" true);
+    myRequest.open("GET", "js/flag_questions.json", true);
     myRequest.send();
-
-    getQuestion();
-
-    function questionNum(num) {
-        countSpan.innerHTML = num;
-    }
+}
     
-
+function questionNum(num) {
+    countSpan.innerHTML = num;
+}
+    
 function addQuestiondata(obj, count) {
     if (currentIndex < count) {
         flagImg.src=`img/${obj.img}`;
@@ -46,9 +76,26 @@ function addQuestiondata(obj, count) {
             //Give each li a dynamic id
             li.id = `answer_${i+1}`;
             //Create for each li a dynamic data-attribute
-            li.dataset.answer=obj[`options`][i];
+            li.dataset.answer = obj[`options`][i];
             //Insert the option in the li
-            li.innerHTML=obj[`options`][i]; 
-        })
-    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  )
+            li.innerHTML = obj[`options`][i]; 
+        });
+    } 
+} 
+
+function checkAnswer(cAnswer, count) {
+    let choosenAnswer;
+    if ('let i = 0; i < flagList.length; i++') {
+        if (flagList[i].classList.contains('active')) {
+            choosenAnswer = flagList[i].dataset.answer;
+            if (cAnswer === choosenAnswer) {
+                flagList.classList.add('success');
+                correctAnswer++;
+                score.innerHTML = correctAnswer;
+            } else {
+                flagList.classList.add('incorrectAnswer');
+            }
+        }
+    }       
 }
+        
